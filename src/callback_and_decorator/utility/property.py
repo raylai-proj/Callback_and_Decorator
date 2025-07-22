da3d4c1abb99ad722b3a14fba26b1d75a967b7d9
raylai <chunjueilai@gmail.com>
2025-07-22 16:04:35 +0800

    feat: add proj_progress class

--- src/callback_and_decorator/utility/property.py
+++ src/callback_and_decorator/utility/property.py
@@ -5,3 +5,11 @@ Author: Chun-Juei Lai
 Email: chunjueilai@gmail.com
 Date: 07/22/2025
 """
+
+
+class proj_progress(object):
+    """A class allows user access attribute with . using @property."""
+
+    def __init__(self, date):
+        """Initialize project progress object with deadline."""
+        self._deadline = date
